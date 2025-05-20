48c6a11b89f6a08fb99ce97a38aca748b3eb940e
Mubarak <adebowaleraji0103@gmail.com>
2025-05-20 10:59:26 +0100

    added

---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+import { useRef, useState } from "react";
+import "./App.css";
+
+export default function App() {
+  const [task, setTask] = useState("");
+  const [allTasks, setAllTasks] = useState([]);
+
+  function addTask(e) {
+    e.preventDefault();
+
+    const todo = {
+      id: Date.now(),
+      task,
+      completed : false
+    } 
+    console.log((todo));
+    
+    setAllTasks((prevTask) => [...prevTask, task]);+
+    setTask ("");
+  }
+
+  return (
+    <div className="app">
+      <h1>ToDo App</h1>
+      <div className="wrapper">
+        <form className="form" onSubmit={addTask}>
+          <input
+            type="text"
+            placeholder="Add a task"
+            value={task}
+            onChange={(e) => setTask(e.target.value)}
+          />
+          <button>Add</button>
+        </form>
+
+        <ul>
+          {allTasks.map((task, id) => (
+            <li key={id}>
+              <div className="sellCheck">
+                <input type="checkbox" name="" id="" />
+                <span>{task}</span>
+              </div>
+
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="currentColor"
+                  d="M7 21q-.825 0-1.412-.587T5 19V6q-.425 0-.712-.288T4 5t.288-.712T5 4h4q0-.425.288-.712T10 3h4q.425 0 .713.288T15 4h4q.425 0 .713.288T20 5t-.288.713T19 6v13q0 .825-.587 1.413T17 21zM17 6H7v13h10zm-7 11q.425 0 .713-.288T11 16V9q0-.425-.288-.712T10 8t-.712.288T9 9v7q0 .425.288.713T10 17m4 0q.425 0 .713-.288T15 16V9q0-.425-.288-.712T14 8t-.712.288T13 9v7q0 .425.288.713T14 17M7 6v13z"
+                />
+              </svg>
+            </li>
+          ))}
+        </ul>
+
+        <div className="btn">
+          <button onClick={() => setAllTasks([])}>clear all</button>
+        </div>
+      </div>
+    </div>
+  );
+}
